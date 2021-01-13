7ed72635c38d207f755bcd0054239ec8fd87b7ff
jingjinting <2895094701@qq.com>
2021-01-13 17:55:22 +0800

    输入信息

--- src/UI/MainUI.java
+++ src/UI/MainUI.java
@@ -8,9 +8,11 @@ import java.sql.SQLException;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainUI implements IFlightService {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
             System.out.println("请输入相应的数字进行操作:");
             System.out.println("按1，录入航班信息");
@@ -21,7 +23,7 @@ public class MainUI implements IFlightService {
             System.out.println("按6，退出系统");
             int choice = sc.nextInt();
             if (choice == 1) {
-                String id = UUID.randomUUID().toString().replace("-", "");
+                String id = UUID.randomUUID().toString().replace("-","");//实现异常的话，删掉toString后面
                 System.out.println("请输入航班编号：");
                 String flightId = sc.next();
                 System.out.println("请输入机型:");
@@ -36,7 +38,28 @@ public class MainUI implements IFlightService {
                 String departureTime = sc.next();
                 Flight flight = new Flight(id, planeType, flightId, currentSeatsNum, departureAirPort, destinationAirPort, departureTime);
                 IFlightService iFlightService = new FlightServiceImpl();
-                iFlightService.insertFlight(flight);
+                try {
+                    iFlightService.insertFlight(flight);
+                } catch (SQLException e) {
+                    String errorMessage = e.getMessage();
+                    System.out.println(errorMessage);
+                    if (errorMessage.startsWith("ORA-12899")) {
+                        //ORA-12899: value too large for column "OPTS"."FLIGHT"."ID" (actual: 32, maximum: 30)
+                        // 按指定模式在字符串查找
+                        String pattern="(\\W)+(\"\\w+\")\\.(\"\\w+\")\\.(\"\\w+\")";
+                        // 创建 Pattern 对象
+                        Pattern r = Pattern.compile(pattern);
+                        // 现在创建 matcher 对象
+                        Matcher m = r.matcher(errorMessage);
+                        if (m.find()) {
+                            String tableName = m.group(3);
+                            String columnName = m.group(4);
+                            System.out.println(tableName + "表的" + columnName + "这一列的值过大，请仔细检查");
+                        } else {
+                            System.out.println("NO MATCH");
+                        }
+                    }
+                }
             }
         }
 
